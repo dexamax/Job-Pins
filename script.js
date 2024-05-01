d190381b76d6db14b2379948c78106829a656aa9
Kathalanthe <hshalika49@gmail.com>
2024-05-01 19:46:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('#article01').addEventListener('click', function() {
+    window.location.href = 'postLink01.html';
+});
+
+document.querySelector('#article02').addEventListener('click', function() {
+    window.location.href = 'postLink02.html';
+});
+
+document.querySelector('#article03').addEventListener('click', function() {
+    window.location.href = 'btnClickTest.html';
+});
+
+document.querySelector('#article04').addEventListener('click', function() {
+    window.location.href = 'btnClickTest.html';
+});
+
+document.querySelector('#article05').addEventListener('click', function() {
+    window.location.href = 'btnClickTest.html';
+});
